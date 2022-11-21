c3b6d395c7396b4b65f855e3d0c575218941d776
Dylan Depass <dylandepass@gmail.com>
2022-11-21 16:39:14 -0500

    fix: remove responsive image block

--- blocks/responsive-image/responsive-image.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-  [...block.querySelectorAll('img')].forEach((img) => {
-    img.loading = 'eager';
-  });
-  [...block.children].forEach((row, i) => {
-    const imgType = (i === 0) ? 'desktop' : 'mobile';
-    row.classList.add(imgType);
-  });
-}
