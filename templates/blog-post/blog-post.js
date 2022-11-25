767db4599dbdc81944edc5b08360eec370cd7735
Dylan Depass <dylandepass@gmail.com>
2022-11-24 20:35:47 -0500

    fix: blog page work and styling

--- templates/blog-post/blog-post.js
+++ templates/blog-post/blog-post.js
@@ -1,5 +1,10 @@
 export default function decorate(main) {
-  const firstDiv = main.querySelector('div:first-of-type');
+  const section = main.querySelector('div:first-of-type');
+
+  const heroImage = section.querySelector('img:first-of-type');
+  if (heroImage) {
+    heroImage.loading = 'eager';
+  }
 
   const container = document.createElement('div');
   container.classList.add('blog-post');
@@ -7,6 +12,19 @@ export default function decorate(main) {
   post.classList.add('blog-post');
   const content = document.createElement('div');
   content.classList.add('content');
+
+  const heroImageContainer = document.createElement('div');
+  heroImageContainer.classList.add('hero');
+  heroImageContainer.append(heroImage.closest('picture'));
+
+  const title = section.querySelector('h1:first-of-type');
+  if (title) {
+    title.classList.add('h3');
+    heroImageContainer.append(title);
+  }
+
+  content.append(heroImageContainer);
+
   const sidebar = document.createElement('div');
   sidebar.classList.add('sidebar');
 
@@ -15,9 +33,9 @@ export default function decorate(main) {
   post.appendChild(content);
   post.appendChild(sidebar);
 
-  content.append(...firstDiv.childNodes);
+  content.append(...section.childNodes);
   container.append(...post.childNodes);
 
-  firstDiv.innerHTML = '';
-  firstDiv.append(container);
+  section.innerHTML = '';
+  section.append(container);
 }
