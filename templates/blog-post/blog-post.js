602f05e99efbd7ca2b6d2a33c2ede40ce7b1d457
Dylan Depass <dylandepass@gmail.com>
2022-11-25 13:07:36 -0500

    feat: start blog post sidebar

--- templates/blog-post/blog-post.js
+++ templates/blog-post/blog-post.js
@@ -1,3 +1,63 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
+function renderSidebar() {
+  return /* html */`
+        <div>
+            <p class="heading">Author</p>
+            <div class="user-card">
+                <img src="assets/images/client/05.jpg" class="avatar" alt="">
+                <a href="" class="authorName">Alison Parker</a>
+                <p class="title">Content Writer</p>
+            </div>
+
+            <p class="heading">Related posts</p>
+            <div class="post-list">
+                <div class="post">
+                    <img src="" alt="">
+                    <div class="ml-3">
+                        <a href="" class="title">Intro to PostCSS</a>
+                        <p class="published-date">November 1, 2022</p>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="" alt="">
+
+                    <div class="ml-3">
+                        <a href="" class="title">Working with Figma</a>
+                        <p class="published-date">November 14, 2022</p>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="" alt="">
+
+                    <div class="ml-3">
+                        <a href="" class="title">Intro to Storybook</a>
+                        <p class="published-date">November 22, 2022</p>
+                    </div>
+                </div>
+            </div>
+
+            <p class="heading">Social sites</p>
+            <ul class="social-buttons">
+                <p class="button-container">
+                    <em>
+                        <a href="" class="button secondary small icon-only"><span class="icon icon-facebook"></a>
+                    </em>
+                </p>
+            </ul>
+
+            <p class="heading">Tagscloud</p>
+            <ul class="tagcloud">
+                <li><a href="" class="">Franklin</a></li>
+                <li><a href="" class="">Javascript</a></li>
+                <li><a href="" class="">Adobe</a></li>
+                <li><a href="" class="">Web Content Management</a></li>
+                <li><a href="" class="">Web Design</a></li>
+            </ul>
+        </div>
+    `;
+}
+
 export default function decorate(main) {
   const section = main.querySelector('div:first-of-type');
 
@@ -28,7 +88,7 @@ export default function decorate(main) {
   const sidebar = document.createElement('div');
   sidebar.classList.add('sidebar');
 
-  sidebar.innerHTML = /* html */'<h3>Metadata</h3>';
+  sidebar.innerHTML = renderSidebar();
 
   post.appendChild(content);
   post.appendChild(sidebar);
@@ -38,4 +98,6 @@ export default function decorate(main) {
 
   section.innerHTML = '';
   section.append(container);
+
+  decorateIcons(section);
 }
