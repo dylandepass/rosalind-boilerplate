0251f79b87f14c884d5fbde5d703a041d6c98e6d
Dylan Depass <dylandepass@gmail.com>
2022-12-15 14:22:30 -0500

    fix: move post css file to same location as build targets

--- gulpfile.js
+++ gulpfile.js
@@ -6,100 +6,69 @@ const sourcemaps = require('gulp-sourcemaps');
 const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 const rename = require('gulp-rename');
-const merge = require('merge-stream');
-const fs = require('fs');
-const path = require('path');
+const header = require('gulp-header');
+
+const generatedHeader = '/* stylelint-disable */\n/* Generated File: Do not edit directly */\n';
 
 /**
  * UTIL TASKS
  */
 const errorMsg = '\x1b[41mError\x1b[0m';
 
-const onError = (err) => {
+function onError(err) {
   gutil.beep();
   gutil.log(`${errorMsg} ${err.toString()}`);
 
   // @ts-ignore
   this.emit('end');
-};
-
-/**
- * Copy variables
- */
-
-function copyLightTheme() {
-  return src('./node_modules/@rosalind/rosalind-dna/dist/css/light/tokens.css')
-    .pipe(rename('light-theme.css'))
-    .pipe(dest('./styles'));
 }
 
-function copyDarkTheme() {
-  return src('./node_modules/@rosalind/rosalind-dna/dist/css/dark/tokens.css')
-    .pipe(rename('dark-theme.css'))
-    .pipe(dest('./styles'));
-}
-
-/**
- * DEV TASKS
- */
-function getFolders(dir) {
-  return fs.readdirSync(dir)
-    .filter((file) => fs.statSync(path.join(dir, file)).isDirectory());
-}
-
-function buildCSS(type) {
-  const srcPath = `./src/styles/${type}`;
-  const destPath = `./${type}`;
-  const folders = getFolders(`./src/styles/${type}`);
-
-  const tasks = folders.map((folder) => src(path.join(srcPath, folder, '*.css'))
+function buildSystem() {
+  return src('./styles/system/index.css')
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(sourcemaps.init())
     .pipe(postcss())
-    .pipe(dest(`${destPath}/${folder}`)));
-
-  return merge(tasks);
-}
-
-function buildBlocksCSS() {
-  return buildCSS('blocks');
-}
-
-function buildTemplateCSS() {
-  return buildCSS('templates');
-}
-
-function stylesDev() {
-  return src('./src/styles/system/index.css')
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(sourcemaps.init())
-    .pipe(postcss())
+    .pipe(header(generatedHeader))
     .pipe(rename('system.css'))
     .pipe(dest('./styles'));
+}
+
+function buildBlocks() {
+  return src('blocks/**/*.post.css')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sourcemaps.init())
+    .pipe(postcss())
+    .pipe(rename((file) => ({
+      dirname: `blocks/${file.dirname}/`,
+      basename: `${file.basename.replace('.post', '')}`,
+      extname: '.css',
+    })))
+    .pipe(header(generatedHeader))
+    .pipe(dest('.'));
+}
+
+function buildTemplates() {
+  return src('templates/**/*.post.css')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sourcemaps.init())
+    .pipe(postcss())
+    .pipe(rename((file) => ({
+      dirname: `templates/${file.dirname}/`,
+      basename: `${file.basename.replace('.post', '')}`,
+      extname: '.css',
+    })))
+    .pipe(header(generatedHeader))
+    .pipe(dest('.'));
 }
 
 function startWatching() {
-  watch('./src/**/*.css', undefined, series(stylesDev, buildBlocksCSS, buildTemplateCSS));
+  watch(['./**/*.post.css', './styles/system/**/*.css'], undefined, series(buildSystem, buildBlocks, buildTemplates));
 }
 
 exports.dev = series(
-  stylesDev,
-  copyLightTheme,
-  copyDarkTheme,
-  buildBlocksCSS,
-  buildTemplateCSS,
+  buildSystem,
+  buildBlocks,
+  buildTemplates,
   startWatching,
 );
 
-/**
- * BUILD TASKS
- */
-function stylesBuild() {
-  return src('./src/styles/index.css')
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(postcss())
-    .pipe(rename('system.css'))
-    .pipe(dest('./styles'));
-}
-
-exports.build = series(stylesBuild);
+exports.build = series(buildSystem, buildBlocks);
