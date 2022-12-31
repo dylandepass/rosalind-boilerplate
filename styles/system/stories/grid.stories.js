74c1adc89e44cffe50b0a76a9d8d2a3cbb5f6e44
Dylan Depass <dylandepass@gmail.com>
2022-12-30 20:08:08 -0500

    fix: various story updates

--- styles/system/stories/grid.stories.js
+++ styles/system/stories/grid.stories.js
@@ -4,122 +4,67 @@ import { decorateIcons } from '../../../scripts/lib-franklin.js';
 import { loadPage } from '../../../scripts/scripts.js';
 import style from '../../system.css';
 
-const GutterTemplate = () => /* html */`<div>
-            <h2>12 column grid</h2>
-            <div class="demo-grid">
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-            </div>
+const GridExamples = () => /* html */`<div>
+    <h2>12 column grid</h2>
+    <div class="twelve-col-grid">
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+    </div>
+    
+    <h2>12 column grid (spans)</h2>
+    <div class="twelve-col-grid">
+      <div class="col span8">Col (Span 8)</div>
+      <div class="col span4">Col (Span 4)</div>
+    </div>
 
-            <h2>12 column grid</h2>
-            <div class="demo-grid">
-              <div class="col span3">3 col span</div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-              <div class="col"></div>
-            </div>
-        </div>`;
+    <h2>Auto Grid</h2>
+    <div class="auto-grid">
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+    </div>
+
+    <h2>Auto Flex</h2>
+    <div class="auto-flex">
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+      <div class="col">Col</div>
+    </div>
+</div>`;
+
 function decorate(block) {
-  console.log(block);
   const grid = document.createElement('div');
-  grid.innerHTML = GutterTemplate();
+  grid.innerHTML = GridExamples();
 
   block.append(grid);
 }
 
-export const Buttons = (args, context) => FranklinTemplate(loadPage, args, context, decorate);
+export const Grid = (args, context) => FranklinTemplate(loadPage, args, context, decorate);
 
-Buttons.parameters = {
+Grid.parameters = {
   path: '/storybook/grid.plain.html',
   selector: 'div',
   index: 0,
 };
 
-Buttons.storyName = 'Buttons';
+Grid.storyName = 'Grid';
 
-export default {
-  title: 'Layout/Grid',
+Grid.args = {
+  content: GridExamples(),
 };
 
-// export const Small = GutterTemplate.bind({});
-// Small.parameters = {
-//   title: 'Layout/Grid/Gutter/Small',
-//   classes: 'gutter-sm',
-// };
-
-// export const Medium = GutterTemplate.bind({});
-// Medium.parameters = {
-//   title: 'Layout/Grid/Gutter/Medium',
-//   classes: 'gutter-md',
-// };
-
-// export const Large = GutterTemplate.bind({});
-// Large.parameters = {
-//   title: 'Layout/Grid/Gutter/Large',
-//   classes: 'gutter-lg',
-// };
-
-// export const XLarge = GutterTemplate.bind({});
-// XLarge.parameters = {
-//   title: 'Layout/Grid/Gutter/Extra Large',
-//   classes: 'gutter-xl',
-// };
-
-// const GridTemplate = (_, context) => {
-//   const { classes, colSizes } = context.parameters;
-//   return /* html */`
-//         <div class="section">
-//           <div class="block-wrapper">
-//             <div class="row visible ${classes}">
-//               ${colSizes.map((size) =>
-//   /* html */`
-//                   <div class="${size}">
-//                     <div>${size}</div>
-//                   </div>`).join('')}
-//             </div>
-//             <div class="row visible">
-//               <div class="col-6">
-//                 <div>col-6</div>
-//               </div>
-//               <div class="col-6">
-//                 <div>col-6</div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>`;
-// };
-
-// export const TwoTwelve = GridTemplate.bind({});
-// TwoTwelve.parameters = {
-//   title: 'Layout/Grid/12 Columns',
-//   colSizes: ['col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1', 'col-1'],
-// };
-
-// export const TwoCol = GridTemplate.bind({});
-// TwoCol.parameters = {
-//   title: 'Layout/Grid/6 Columns',
-//   colSizes: ['col-2', 'col-2', 'col-8'],
-// };
-
-// export const EightColCenter = GridTemplate.bind({});
-// EightColCenter.parameters = {
-//   title: 'Layout/Grid/8 Columns Center',
-//   classes: 'justify-content-center',
-//   colSizes: ['col-8'],
-// };
+export default {
+  title: 'CSS Mixins/Grid',
+};
