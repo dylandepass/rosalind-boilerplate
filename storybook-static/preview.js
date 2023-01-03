4d6dfcd33a0f5021f4e0761c1071c2a5a9cb3021
Dylan Depass <dylandepass@gmail.com>
2023-01-03 14:36:17 -0500

    feat: update storybook

--- storybook-static/preview.js
+++ storybook-static/preview.js
@@ -1,9 +1,23 @@
+import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme'
+
+import light from '!!style-loader?injectType=lazyStyleTag!css-loader!../styles/themes/rosalind-light.css'
+import dark from '!!style-loader?injectType=lazyStyleTag!css-loader!../styles/themes/rosalind-dark.css'
+import WKND from '!!style-loader?injectType=lazyStyleTag!css-loader!../styles/themes/WKND-light.css'
+import vader from '!!style-loader?injectType=lazyStyleTag!css-loader!../styles/themes/Vader.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+  host: 'https://main--rosalind-boilerplate--dylandepass.hlx.page',
+  cssVariables: {
+    files: {
+      'rosalind-light': light,
+      'rosalind-dark': dark,
+      'WKND-light': WKND,
+      'Vader': vader,
     },
-  },
-}
+    defaultTheme: 'rosalind-light'
+  }
+};
+
+export const decorators = [
+  cssVariablesTheme,
+];
