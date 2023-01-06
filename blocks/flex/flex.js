2b26d8d91bd96ad99f3788e7c009eb2cce856762
Dylan Depass <dylandepass@gmail.com>
2023-01-05 22:33:42 -0500

    fix: remove auto grid and auto flex

--- blocks/flex/flex.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  if (block.classList.contains('full-width-section')) {
-    block.classList.remove('full-width-section');
-    block.parentElement.parentElement.classList.add('full-width-section');
-  }
-}
