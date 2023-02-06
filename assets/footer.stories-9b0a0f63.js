51b72eabba0eaad05d757d5791d13f2906f9c7a6
dylandepass <dylandepass@gmail.com>
2023-02-06 20:13:09 +0000

    Deploying to gh-pages from  @ 70ad723e371465c1f0ff273995feca5e560aed50 🚀

---
+++ assets/footer.stories-9b0a0f63.js
@@ -0,0 +1,11 @@
+import{i as a,j as n,k as i}from"./lib-franklin-4fe0dc03.js";import"./preload-helper-41c905a7.js";const d={title:"Blocks/Footer",parameters:{layout:"fullscreen"}},o={render:(s,e)=>{const t=document.createElement("footer"),r=a("footer","");return t.append(r),n(r),i(r,e.parameters.host),t}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: (args, context) => {
+    const footer = document.createElement('footer');
+    const footerBlock = buildBlock('footer', '');
+    footer.append(footerBlock);
+    decorateBlock(footerBlock);
+    loadBlock(footerBlock, context.parameters.host);
+    return footer;
+  }
+}`,...o.parameters?.docs?.source}}};const c=["Footer"];export{o as Footer,c as __namedExportsOrder,d as default};
+//# sourceMappingURL=footer.stories-9b0a0f63.js.map
