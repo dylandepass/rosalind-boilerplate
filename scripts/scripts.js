a79cc62a562efccf252c54812f992280ad057ab4
dylandepass <dylandepass@gmail.com>
2023-02-06 10:42:18 -0500

    feat: Initial block library (#24)
    * feat: initial library
    
    * Design System Build
    
    * fix: update library
    
    * Design System Build
    
    ---------
    
    Co-authored-by: dylandepass <dylandepass@users.noreply.github.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,6 +27,27 @@ function build2ColHero(main) {
   main.prepend(section);
 }
 
+export function createTag(tag, attributes, html) {
+  const el = document.createElement(tag);
+  if (html) {
+    if (html instanceof HTMLElement
+      || html instanceof SVGElement
+      || html instanceof DocumentFragment) {
+      el.append(html);
+    } else if (Array.isArray(html)) {
+      el.append(...html);
+    } else {
+      el.insertAdjacentHTML('beforeend', html);
+    }
+  }
+  if (attributes) {
+    Object.entries(attributes).forEach(([key, val]) => {
+      el.setAttribute(key, val);
+    });
+  }
+  return el;
+}
+
 /**
  * Loads JS and CSS for a template.
  */
