589a6603d8d089f807c721707dc23a2d192a29be
Dylan Depass <dylandepass@gmail.com>
2023-02-10 22:28:35 -0500

    fix: add code coverage + ci tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   stories: ['../docs', '../blocks/**/*.mdx', '../blocks/**/*.stories.@(js|jsx|ts|tsx)', '../templates/**/*.stories.@(js|jsx|ts|tsx)', '../styles/system/stories/**/*.stories.@(js|jsx|ts|tsx)', '../styles/system/stories/**/*.mdx'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@dylandepass/franklin-storybook-addon'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-coverage', '@dylandepass/franklin-storybook-addon'],
   framework: {
     name: '@storybook/html-vite',
     options: {},
--- docs/stories/components/buttons.stories.js
+++ docs/stories/components/buttons.stories.js
@@ -1,4 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, no-unused-vars */
+import { within, waitFor, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { FranklinTemplate } from '@dylandepass/franklin-storybook-addon';
 import { decorateIcons } from '../../../scripts/lib-franklin.js';
 import { loadPage } from '../../../scripts/scripts.js';
@@ -43,4 +45,36 @@ export const Buttons = {
     index: 0,
   },
   render: (args, context) => FranklinTemplate(loadPage, args, context, decorate),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await waitFor(() => {
+      expect(canvas.getByText('Buttons')).toBeInTheDocument();
+    });
+
+    const primary = canvas.getByTitle('Primary');
+    expect(primary).toBeInTheDocument();
+
+    async function testButtonByTitle(title) {
+      const btn = canvas.getByTitle(title);
+      expect(btn).toBeInTheDocument();
+      if (title.includes('Icon')) {
+        expect(btn.querySelector('.icon')).toBeInTheDocument();
+        await waitFor(() => {
+          expect(btn.querySelector('svg')).toBeInTheDocument();
+        });
+      }
+    }
+
+    testButtonByTitle('Primary');
+    testButtonByTitle('Primary Icon Left');
+    testButtonByTitle('Primary Icon Right');
+
+    testButtonByTitle('Secondary');
+    testButtonByTitle('Secondary Icon Left');
+    testButtonByTitle('Secondary Icon Right');
+
+    testButtonByTitle('Quiet');
+    testButtonByTitle('Quiet Icon Left');
+    testButtonByTitle('Quiet Icon Right');
+  },
 };
