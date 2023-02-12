c2a10cd38fc51347855dbc8e7083d7372d696c83
dylandepass <dylandepass@gmail.com>
2023-02-12 04:05:33 +0000

    Deploying to gh-pages from  @ 353c65ee712cd4b18aa710f57f2e2c91dedf5d9e 🚀

---
+++ assets/grid.stories-4b69208b.js
@@ -0,0 +1,49 @@
+import{e}from"./index-dd503cea.js";import{l as o}from"./scripts-9fb39b32.js";import s from"./grid-9a171a29.js";import"./preload-helper-41c905a7.js";import"./lib-franklin-14c67339.js";const T={title:"Blocks/Grid"},g={path:"/storybook/blocks/grid/two-cols.plain.html",selector:".grid",index:0},n={parameters:g,render:(r,a)=>e.FranklinTemplate(o,r,a,s)},t={parameters:{...g,viewport:{defaultViewport:"xs"}},render:(r,a)=>e.FranklinTemplate(o,r,a,s)},h={path:"/storybook/blocks/grid/icon-cards.plain.html",selector:".grid",index:0},i={parameters:h,render:(r,a)=>e.FranklinTemplate(o,r,a,s)},d={parameters:{...h,viewport:{defaultViewport:"xs"}},render:(r,a)=>e.FranklinTemplate(o,r,a,s)},u={path:"/storybook/blocks/grid/logo-grid.plain.html",selector:".grid",index:0},c={parameters:u,render:(r,a)=>e.FranklinTemplate(o,r,a,s)},m={parameters:{...u,viewport:{defaultViewport:"xs"}},render:(r,a)=>e.FranklinTemplate(o,r,a,s)},x={path:"/storybook/blocks/grid/bulleted.plain.html",selector:".grid",index:0},p={parameters:x,render:(r,a)=>e.FranklinTemplate(o,r,a,s)},l={parameters:{...x,viewport:{defaultViewport:"xs"}},render:(r,a)=>e.FranklinTemplate(o,r,a,s)};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  parameters: TwoColsParams,
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...TwoColsParams,
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: (args, context) => {
+    const res = FranklinTemplate(loadPage, args, context, decorate);
+    return res;
+  }
+}`,...t.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  parameters: IconCardParams,
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...i.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...IconCardParams,
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...d.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  parameters: LogoGridParams,
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...c.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...LogoGridParams,
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...m.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  parameters: BulletedParams,
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...p.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...BulletedParams,
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: (args, context) => FranklinTemplate(loadPage, args, context, decorate)
+}`,...l.parameters?.docs?.source}}};const P=["TwoCols","TwoColsMobile","IconCards","IconCardsModile","LogoGrid","LogoGridMobile","Bulleted","BulletedMobile"];export{p as Bulleted,l as BulletedMobile,i as IconCards,d as IconCardsModile,c as LogoGrid,m as LogoGridMobile,n as TwoCols,t as TwoColsMobile,P as __namedExportsOrder,T as default};
+//# sourceMappingURL=grid.stories-4b69208b.js.map
--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const path = require('path');
+const istanbul = require('vite-plugin-istanbul');
+const constants = require('@storybook/addon-coverage/dist/cjs/constants');
+
 module.exports = {
   stories: ['../docs', '../blocks/**/*.mdx', '../blocks/**/*.stories.@(js|jsx|ts|tsx)', '../templates/**/*.stories.@(js|jsx|ts|tsx)', '../styles/system/stories/**/*.stories.@(js|jsx|ts|tsx)', '../styles/system/stories/**/*.mdx'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-coverage', '@dylandepass/franklin-storybook-addon'],
+  addons: ['@storybook/addon-a11y', 
+           '@storybook/addon-links', 
+           '@storybook/addon-essentials', 
+           '@storybook/addon-interactions', 
+           '@dylandepass/franklin-storybook-addon', 
+           {
+              name: "@storybook/addon-coverage",
+              options: {
+                istanbul: {
+                  exclude: ['**/lib-franklin.js', '**/scripts.js']
+                },
+              },
+           },
+  ],
   framework: {
     name: '@storybook/html-vite',
     options: {},
