a04100be81c600da52cb926462c66de93a4a0ca0
dylandepass <dylandepass@gmail.com>
2023-02-13 05:28:57 +0000

    Deploying to gh-pages from  @ c2e3b56f7b3e0f133882d683af8b7383e0f8d4a4 🚀

---
+++ assets/color-53024228.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as d}from"./index-a10c4dc9.js";import{T as i,a as r}from"./index-ff8b6259.js";import{t as n}from"./rosalind-light-374e77ca.js";import{u as c}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-a224d31d.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./_baseToString-53b55ec4.js";import"./_getTag-7cbcf524.js";import"./isEqual-d0561132.js";import"./index-7fb38eb9.js";import"./uniq-74fc131e.js";import"./index-356e4a49.js";function O(s={}){const{wrapper:t}=Object.assign({},c(),s.components);return t?e.jsx(t,Object.assign({},s,{children:e.jsx(l,{})})):l();function l(){const o=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},c(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(d,{title:"Docs/Developers/Tokens/Color"}),`
+`,e.jsx(o.h1,{children:"Colors"}),`
+`,e.jsxs(o.blockquote,{children:[`
+`,e.jsx(o.p,{children:"The Rosalind Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(o.h2,{children:"Primary Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.primary,prefix:"--ros-semantic-color-primary"})]}),`
+`,e.jsx(o.h2,{children:"Secondary Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.secondary,prefix:"--ros-semantic-color-secondary"})]}),`
+`,e.jsx(o.h2,{children:"Background Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.background,prefix:"--ros-semantic-color-background"})]}),`
+`,e.jsx(o.h2,{children:"Foreground Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.foreground,prefix:"--ros-semantic-color-foreground"})]}),`
+`,e.jsx(o.h2,{children:"Utility Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(r,{k:"default",value:n.semantic.color.positive.default,prefix:"--ros-semantic-color-positive-default"}),e.jsx(r,{k:"default",value:n.semantic.color.negative.default,prefix:"--ros-semantic-color-negative-default"}),e.jsx(r,{k:"default",value:n.semantic.color.notice.default,prefix:"--ros-semantic-color-notice-default"}),e.jsx(r,{k:"default",value:n.semantic.color.informative.default,prefix:"--ros-semantic-color-informative-default"})]}),`
+`,e.jsx(o.h2,{children:"Link Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.link,prefix:"--ros-semantic-color-link"})]}),`
+`,e.jsx(o.h2,{children:"Seperator Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.seperator,prefix:"--ros-semantic-color-seperator"})]}),`
+`,e.jsx(o.h2,{children:"Seperator Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.semantic.color.seperator,prefix:"--ros-semantic-color-seperator"})]}),`
+`,e.jsx(o.h2,{children:"Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:n.theme.color,prefix:"--ros-theme-color"})]})]})}}export{O as default};
+//# sourceMappingURL=color-53024228.js.map
---
+++ assets/size-a1d3dc77.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as m}from"./index-a10c4dc9.js";import{a as r,T as c}from"./index-ff8b6259.js";import{t}from"./rosalind-light-374e77ca.js";import{u as a}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-a224d31d.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./_baseToString-53b55ec4.js";import"./_getTag-7cbcf524.js";import"./isEqual-d0561132.js";import"./index-7fb38eb9.js";import"./uniq-74fc131e.js";import"./index-356e4a49.js";function R(i={}){const{wrapper:s}=Object.assign({},a(),i.components);return s?e.jsx(s,Object.assign({},i,{children:e.jsx(o,{})})):o();function o(){const n=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},a(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Docs/Developers/Tokens/Size"}),`
+`,e.jsx(n.h1,{children:"Sizes"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The Rosalind Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Section Size Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(r,{k:"max-width",value:t.semantic.size.section["max-width"],prefix:"--ros-semantic-size-section"})]}),`
+`,e.jsx(n.h2,{children:"Default Content Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(r,{k:"margin-bottom",value:t.semantic.size["default-content-wrapper"]["margin-bottom"],prefix:"--ros-semantic-size-default-content-wrapper"})]}),`
+`,e.jsx(n.h2,{children:"Breakpoint Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(c,{tokens:t.semantic.size.breakpoints,prefix:"--ros-semantic-size-breakpoints"})]})]})}}export{R as default};
+//# sourceMappingURL=size-a1d3dc77.js.map
