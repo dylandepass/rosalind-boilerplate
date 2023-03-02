6d04e4a1ebae9fc10dc01c275063acbe5c4d047d
Dylan Depass <dylandepass@gmail.com>
2023-03-02 10:48:31 -0500

    fix: update storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'plugin:storybook/recommended'],
   env: {
-    browser: true,
+    browser: true
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
     allowImportExportEverywhere: true,
     sourceType: 'module',
-    requireConfigFile: false,
+    requireConfigFile: false
   },
   rules: {
     // allow reassigning param
-    'no-param-reassign': [2, { props: false }],
+    'no-param-reassign': [2, {
+      props: false
+    }],
     'linebreak-style': ['error', 'unix'],
     'import/extensions': ['error', {
-      js: 'always',
-    }],
-  },
+      js: 'always'
+    }]
+  }
 };
