e80e6cf13a7f4193ee0415b2d2fca6a2dd0eb5b9
dylandepass <dylandepass@gmail.com>
2023-03-13 14:55:16 -0400

    fix: update banner logic (#60)
    * fix: add overlay background color
    
    * fix: build system
    
    * Design System Build
    
    * fix: don't run storybook tests `during npm run test`
    
    * fix: update banner logic and sytling
    
    ---------
    
    Co-authored-by: dylandepass <dylandepass@users.noreply.github.com>

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -12,31 +12,49 @@
 
 /** @param {HTMLDivElement} block */
 export default function decorate(block) {
+  // Add `full-width` class to parent section
   const section = block.closest('.section');
-
   section.classList.add('full-width');
 
+  // First column should be the hero image.. Add class
   const image = block.querySelector(':scope > div > div:nth-of-type(1)');
   if (image) {
     image.className = 'bg-image';
   }
 
-  const bannerContentContainer = block.querySelector(':scope > div > div:nth-of-type(2)');
+  const contentFullWidthSectionWrapper = document.createElement('div');
+  contentFullWidthSectionWrapper.classList.add('section');
+  contentFullWidthSectionWrapper.classList.add('full-width-section');
 
-  if (bannerContentContainer) {
-    bannerContentContainer.className = 'container';
+  const contentFullWidthSectionDiv = document.createElement('div');
+  contentFullWidthSectionWrapper.append(contentFullWidthSectionDiv);
+
+  // Second column should be the content..  add container class
+  const heroContentContainer = block.querySelector(':scope > div > div:nth-of-type(2)');
+  if (heroContentContainer) {
+    heroContentContainer.className = 'container';
   }
 
+  // We need to put the content inside any div so if can float to the correct position
   const content = document.createElement('div');
   content.classList.add('content');
-  content.innerHTML = bannerContentContainer.innerHTML;
 
-  bannerContentContainer.innerHTML = '';
-  bannerContentContainer.appendChild(content);
+  contentFullWidthSectionDiv.append(content);
 
+  // Append the content to the new div
+  content.innerHTML = heroContentContainer.innerHTML;
+
+  // Empty out the old children
+  heroContentContainer.innerHTML = '';
+
+  // Append on our new div to the container
+  heroContentContainer.appendChild(contentFullWidthSectionWrapper);
+
+  // Wrap out buttons in a group
   const buttonGroup = document.createElement('div');
   buttonGroup.className = 'button-group';
 
+  // Search through the buttons and find the primary ones
   block.querySelectorAll('a').forEach((a) => {
     const container = a.closest('.button-container');
     if (a.classList.contains('primary')) {
@@ -46,5 +64,6 @@ export default function decorate(block) {
     buttonGroup.appendChild(a.closest('.button-container'));
   });
 
+  // Append the button group
   content.append(buttonGroup);
 }
